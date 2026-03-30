0382e546fa2bfe67f2c52f75976fd9f8385b272d
Harshil-1603 <harshil.agrawal098@gmail.com>
2026-03-31 01:30:24 +0530

    Delete train.py

--- train.py
+++
@@ -1,7 +0,0 @@
-from sklearn.cluster import KMeans
-
-def dominant_colors(image, k=3):
-    pixels = image.reshape(-1, 3)
-    kmeans = KMeans(n_clusters=k)
-    kmeans.fit(pixels)
-    return kmeans.cluster_centers_
